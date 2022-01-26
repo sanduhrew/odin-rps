6f500c92fd52a5cba2714c3a4c8d7ca4365687bd
Drew <onemountain@gmail.com>
2022-01-26 11:01:27 +0900

    haggard v1.0 complete w comments

--- rps.js
+++ rps.js
@@ -19,19 +19,33 @@ function computerPlay (min, max) {
     return compPlay;
 }
 
+const compSelect = computerPlay(1,3);
+
+/*function playRound (playerSelection, compSelect)  {
+    return compSelect === playerSelection ? 'T'
+    :  compSelect === 'ROCK' && playerSelection === 'SCISSORS' ? 'L'
+    :  compSelect === 'PAPER' && playerSelection === 'ROCK' ? 'L' 
+    :  compSelect === 'SCISSORS' && playerSelection === 'PAPER' ? 'L'
+    :  playerSelection === 'ROCK' && compSelect === 'SCISSORS' ? 'W'
+    :  playerSelection === 'PAPER' && compSelect === 'ROCK' ? 'W'
+    :  playerSelection === 'SCISSORS' && compSelect === 'PAPER' ? 'W'
+    :  'Invalid play. Please try again.'
+} */
+
+
 function playRound (playerSelection, compSelect)  {
-    playerSelection = playerSelection.toUpperCase();
     return compSelect === playerSelection ? 'It is a tie. Play again!'
-    :  compSelect === 'ROCK' && playerSelection === 'SCISSORS' ? 'You lose. Rock beats scissors'
+    :  compSelect === 'ROCK' && playerSelection === 'SCISSORS' ? 'You lose. Rock beats scissors.'
     :  compSelect === 'PAPER' && playerSelection === 'ROCK' ? 'You lose. Paper beats rock.' 
     :  compSelect === 'SCISSORS' && playerSelection === 'PAPER' ? 'You lose. Scissors beat paper.'
     :  playerSelection === 'ROCK' && compSelect === 'SCISSORS' ? 'You win! Rock beats scissors.'
-    :  playerSelection === 'PAPER' && compSelect === 'ROCK' ? 'You win! Paper beats rock'
+    :  playerSelection === 'PAPER' && compSelect === 'ROCK' ? 'You win! Paper beats rock.'
     :  playerSelection === 'SCISSORS' && compSelect === 'PAPER' ? 'You win! Scissors beat rock.'
     :  'Invalid play. Please try again.'
-}
+} 
 
-/* fml forgot to pass the numbers through computerPlay and wrote it again. Think if else is
+
+/* fml forgot to pass the numbers 1,3 through computerPlay and wrote it again. Think if else is
 eaiser to read anyway
 
 function playRound (playerSelection, compSelect)  {
@@ -59,6 +73,70 @@ function playRound (playerSelection, compSelect)  {
     }
 } */
 
-const playerSelection = 'ROCK';
+
+/* 
 const compSelect = computerPlay(1,3);
+const playerSelection = 'rock'; 
 console.log(playRound(playerSelection, compSelect));
+this was working. why can't game() see playRound?? scope stack hoist wtf? 
+need a whisky
+*/
+
+let playerWins = 0
+let computerWins = 0
+let ties = 0
+let playerSelection
+
+/* Could change the long strings in playRound to simple single characters. 
+   Could also declare global const RL PL SL RW PW SW = e.g. 'You lose. Rock beats scissors.' Use those to avoid time consuming spelling issues, fk. 
+   Use those within game()
+
+function game(playRound) { 
+    if (playRound === 'W') {
+        playerWins = playerWins + 1; 
+        console.log('You win!');
+    }
+    else if (playRound === 'L'){
+        computerWins = computerWins + 1;
+        console.log('You lose!')
+    }
+    else if (playRound === 'T') {
+        ties = ties + 1;
+        console.log('It\'s a tie.')
+    }
+    console.log('Player wins: ' + playerWins);
+    console.log('Computer wins: ' + computerWins);
+    console.log('Ties:' + ties);
+} */
+
+function game(playRound) { 
+    if (playRound === 'You win! Rock beats scissors.' || playRound === 'You win! Paper beats rock.' || playRound === 'You win! Scissors beat rock.') {
+        playerWins = playerWins + 1; 
+    }
+    else if (playRound === 'You lose. Rock beats scissors.' || playRound === 'You lose. Paper beats rock.' || playRound === 'You lose. Scissors beat paper.') {
+        computerWins = computerWins + 1;
+    }
+    else if (playRound === 'It is a tie. Play again!') {
+        ties = ties + 1;
+    }
+    console.log(playRound);
+    console.log('Player wins: ' + playerWins);
+    console.log('Computer wins: ' + computerWins);
+    console.log('Ties:' + ties);
+}
+
+for (let gameCount = 0; gameCount < 5; gameCount++){
+    playerPrompt = prompt('Enter RPS'); 
+    let playerSelection = playerPrompt.toUpperCase();
+    console.log('Round '+ (gameCount + 1));
+    console.log(game(playRound(playerSelection, compSelect)));
+}  
+console.log('Final Results: ')
+if (playerWins > computerWins) {
+    console.log('You win. Congrats.');
+}
+    else if (playerWins === computerWins) {
+      console.log('It\'s a tie. Try again.');
+    }
+    else {console.log('You lost. Better luck next time, sucker.')
+}
