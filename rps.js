77b23264c4963bbe91314d3c8cf8f2792b0eea9a
Drew <onemountain@gmail.com>
2022-02-09 17:07:19 +0900

    nested functions, works now

--- rps.js
+++ rps.js
@@ -1,14 +1,11 @@
-
 const ROCK = 'ROCK'
 const PAPER = 'PAPER'
 const SCISSORS = 'SCISSORS'
 
-const compSelect = computerPlay(1,3);
-
 function computerPlay (min, max) {
     let play;
     let compPlay;
-    play = Math.floor(Math.random() * (max-min + 1)) + min;
+    play = Math.floor(Math.random() * (max - min + 1) + min);
       if (play === 1) {
           compPlay = ROCK;
       }
@@ -28,10 +25,37 @@ const scissorsLose = 'You lose. Scissors beat paper.';
 const rockWin = 'You win! Rock beats scissors.';
 const paperWin = 'You win! Paper beats rock.';
 const scissorsWin = 'You win! Scissors beat paper.';
+const invalid = 'Invalid play. Try again.'
 
-let playerSelection; //defined below in gameCount for loop from window.prompt
+let playerSelection; //user defined below from window.prompt
 
 function playRound (playerSelection, compSelect)  {
+    if (compSelect === playerSelection) {
+        return tieGame;
+    }
+    else if (compSelect === ROCK && playerSelection === SCISSORS) {
+        return rockLose; 
+    }
+    else if (compSelect === PAPER && playerSelection === ROCK) {
+        return paperLose;
+    }
+    else if (compSelect === SCISSORS && playerSelection === PAPER) {
+        return scissorsLose;
+    }
+    else if (playerSelection === ROCK && compSelect === SCISSORS) {
+        return rockWin; 
+    }
+    else if (playerSelection === PAPER && compSelect === ROCK) {
+        return paperWin;
+    }
+    else if (playerSelection === SCISSORS && compSelect === PAPER) {
+        return scissorsWin; 
+    }
+    else {return invalid;
+    }      
+}
+
+/* function playRound (playerSelection, compSelect)  {
     return compSelect === playerSelection ? tieGame
     :  compSelect === ROCK && playerSelection === SCISSORS ? rockLose
     :  compSelect === PAPER && playerSelection === ROCK ? paperLose
@@ -39,42 +63,54 @@ function playRound (playerSelection, compSelect)  {
     :  playerSelection === ROCK && compSelect === SCISSORS ? rockWin
     :  playerSelection === PAPER && compSelect === ROCK ? paperWin
     :  playerSelection === SCISSORS && compSelect === PAPER ? scissorsWin
-    :  'Invalid play. Please try again.'
+    :  invalid
     }   
-    
+*/
+
 let playerWins = 0
 let computerWins = 0
 let ties = 0
+let roundWinner;
 
-function game(playRound) { 
-    if (playRound === 'You win! Rock beats scissors.' || playRound === 'You win! Paper beats rock.' || playRound === 'You win! Scissors beat rock.') {
-        playerWins = playerWins + 1; 
-    }
-    else if (playRound === 'You lose. Rock beats scissors.' || playRound === 'You lose. Paper beats rock.' || playRound === 'You lose. Scissors beat paper.') {
-        computerWins = computerWins + 1;
-    }
-    else if (playRound === 'It is a tie. Play again!') {
-        ties = ties + 1;
-    }
-    console.log(playRound);
-    console.log('Player wins: ' + playerWins);
-    console.log('Computer wins: ' + computerWins);
+function game() { 
+    for (let gameCount = 0; gameCount < 5; gameCount++) {
+        playerPrompt = prompt('Enter RPS'); 
+        let compSelect = computerPlay(1,3);
+        let playerSelection = playerPrompt.toUpperCase();
+        console.log('// Round '+ (gameCount + 1)); 
+        console.log('Player choice: ' + playerSelection);
+        console.log('Computer choice: '+ compSelect); 
+        roundWinner = playRound(playerSelection, compSelect);
+        console.log(roundWinner); 
+
+        if (roundWinner === rockWin || roundWinner === paperWin || roundWinner === scissorsWin) {
+            playerWins++; 
+        }
+        else if (roundWinner === rockLose || roundWinner === paperLose || roundWinner === scissorsLose) {
+            computerWins++;
+        }
+        else if (roundWinner === tieGame) {
+            ties++;
+        }
+        console.log('Player wins: ' + playerWins);
+        console.log('Computer wins: ' + computerWins);
+        console.log('Ties:' + ties);
+    } 
+
+    console.log('Final Results: ');
+    console.log('TOTAL Player wins: ' + playerWins);
+    console.log('TOTAL Computer wins: ' + computerWins);
     console.log('Ties:' + ties);
-}
 
-for (let gameCount = 0; gameCount < 5; gameCount++){
-    playerPrompt = prompt('Enter RPS'); 
-    let playerSelection = playerPrompt.toUpperCase();
-    console.log('Round '+ (gameCount + 1));
-    console.log(game(playRound(playerSelection, compSelect)));
-}  
-
-console.log('Final Results: ')
-if (playerWins > computerWins) {
-    console.log('You win. Congrats.');
-}
-    else if (playerWins === computerWins) {
-      console.log('It\'s a tie. Try again.');
+    if (playerWins > computerWins) {
+        console.log('You win. Congrats.');
     }
-    else {console.log('You lost. Better luck next time, sucker.')
+    else if (playerWins === computerWins) {
+        console.log('It\'s a tie. Try again.');
+    }
+    else {
+        console.log('You lost. Better luck next time, sucker.');
+    }
 }
+
+game(); 
